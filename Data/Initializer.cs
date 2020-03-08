1fddb94f125440c23ff20283c427f75eebf2ffb2
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-03-08 16:30:44 +0100

    vorderingen mapping json

--- Data/Initializer.cs
+++ Data/Initializer.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BackEndRemiMestdagh.Data
 {
@@ -17,7 +18,7 @@ namespace BackEndRemiMestdagh.Data
         public void InitializeData()
         {
 
-            using (StreamReader r = new StreamReader(@"C:\Users\remi\PycharmProjects\scraper2\scraper\films2000.json"))
+            using (StreamReader r = new StreamReader(@"C:\Users\Remi\Desktop\films2000.json"))
             {
                 string json = r.ReadToEnd();
 
@@ -31,9 +32,8 @@ namespace BackEndRemiMestdagh.Data
                 _context.Database.EnsureDeleted();
                 if (_context.Database.EnsureCreated())
                 {
-                    HashSet<Acteur> acteurs = new HashSet<Acteur>();
-                    HashSet<Genre> genres = new HashSet<Genre>();
-                   
+
+
                     HashSet<Film> deFilms = new HashSet<Film>();
                     HashSet<Regisseur> deRegisseur = new HashSet<Regisseur>();
 
@@ -41,88 +41,120 @@ namespace BackEndRemiMestdagh.Data
                     HashSet<string> deRegisseursString = new HashSet<string>();
                     HashSet<string> deGenresString = new HashSet<string>();
                     HashSet<string> deActeursString = new HashSet<string>();
-
+                    HashSet<Acteur> acteurs = new HashSet<Acteur>();
+                    HashSet<Genre> genres = new HashSet<Genre>();
                     foreach (MockObject m in films)
                     {
-                        
-
+                        HashSet<string> deGenresStringVanFilm = new HashSet<string>();
+                        HashSet<string> deActeursStringFilm = new HashSet<string>();
+                        HashSet<Acteur> acteursVanFilm = new HashSet<Acteur>();
+                        HashSet<Genre> genresVanFilm = new HashSet<Genre>();
+                        Regisseur regisseurVanFilm;
                         string[] tempact = m.stars.Split(",");
-
                         string regisseurString = tempact[0];
                         deRegisseursString.Add(regisseurString);
-                        Regisseur saveRegi = new Regisseur(regisseurString);
+                        if (deRegisseursString.Contains(regisseurString)&&deRegisseur.Select(d=>d.Naam).Contains(regisseurString))
+                        {
+                            regisseurVanFilm = deRegisseur.First(r => r.Naam.Equals(regisseurString));
+                        }
+                        else
+                        {
+                            regisseurVanFilm = new Regisseur(regisseurString);
+                            deRegisseur.Add(regisseurVanFilm);
+                        }
+                        
+
                         for (int i = 1; i < tempact.Length - 2; i++)
                         {
-                            
                             deActeursString.Add(tempact[i]);
+
+                            deActeursStringFilm.Add(tempact[i]);
                         }
 
                         string[] tempgenres = m.genres.Split(", ");
                         for (int i = 0; i < tempgenres.Length - 1; i++)
                         {
-                            deGenresString.Add(tempgenres[i]);
+                            deGenresString.Add(tempact[i]);
+
+                            deGenresStringVanFilm.Add(tempgenres[i]);
                         }
                         string[] tempruntime = m.runtime.Split(" ");
                         string formattedRuntime = "";
-
                         formattedRuntime = tempruntime[0];
-
-
-
-
                         Film nieuweFilm = new Film()
                         {
                             Titel = m.titel,
                             Score = double.Parse(m.score),
-                            Regisseur = saveRegi,
+                            Regisseur = regisseurVanFilm,
                             TitleImage = m.titleImage,
                             Runtime = double.Parse(formattedRuntime),
                             Year = int.Parse(m.year)
 
                         };
-                        
-
-                        foreach (string acteur in deActeursString)
-                        {
-                            Acteur saveAct = new Acteur(acteur);
-                            acteurs.Add(saveAct);
-                            _context.Acteurs.Add(saveAct);
-
-                        }
-                        foreach (string genre in deGenresString)
-                        {
-                            Genre saveGenre = new Genre(genre);
-                            genres.Add(saveGenre);
-                            _context.Genres.Add(saveGenre);
-                        }
-                        foreach (string regisseur in deRegisseursString)
-                        {
-                            deRegisseur.Add(saveRegi);
-                            _context.Regisseurs.Add(saveRegi);
-                        }
                         List<ActeurFilm> acteurFilms = new List<ActeurFilm>();
                         List<GenreFilm> genreFilms = new List<GenreFilm>();
-                        foreach (Acteur acteur in acteurs)
+                        foreach (string acteur in deActeursStringFilm)
                         {
-                            acteurFilms.Add(new ActeurFilm(nieuweFilm, acteur));
+                            Acteur acteur1;
+                            if (deActeursString.Contains(acteur) && acteurs.Select(s => s.Naam).Contains(acteur))
+                            {
+                                acteur1 = acteurs.First(a => a.Naam.Equals(acteur)) ?? null;
+                            }
+                            else
+                            {
+                                acteur1 = new Acteur(acteur);
+                                acteurs.Add(acteur1);
+                                deActeursString.Add(acteur);
+                            }
+                            acteursVanFilm.Add(acteur1);
+                            acteurFilms.Add(new ActeurFilm(nieuweFilm, acteur1));
+
+
                         }
-                        foreach (Genre genre in genres)
+                        foreach (string genre in deGenresStringVanFilm)
                         {
-                            genreFilms.Add(new GenreFilm(nieuweFilm, genre));
+                            Genre saveGenre;
+                            if (deGenresString.Contains(genre) && genres.Select(g => g.Naam).Contains(genre))
+                            {
+                                saveGenre = genres.First(g => g.Naam.Equals(genre)) ?? null;
+                            }
+                            else
+                            {
+                                saveGenre = new Genre(genre);
+                                genres.Add(saveGenre);
+                                deGenresString.Add(genre);
+                            }
+
+                            genresVanFilm.Add(saveGenre);
+                            genreFilms.Add(new GenreFilm(nieuweFilm, saveGenre));
+
                         }
                         nieuweFilm.Acteurs = acteurFilms;
                         nieuweFilm.Genres = genreFilms;
                         deFilms.Add(nieuweFilm);
+
                     }
 
 
 
                     Console.WriteLine(deFilms.Count);
                     Console.WriteLine(acteurs.Count);
-                    Console.WriteLine(deRegisseur.Count);
                     Console.WriteLine(genres.Count);
 
+
+
+
+                    _context.Regisseurs.AddRange(deRegisseur);
+                    _context.Acteurs.AddRange(acteurs);
+                    _context.Genres.AddRange(genres);
                     _context.Films.AddRange(deFilms);
+
+
+
+
+
+
+
                 }
 
             }
